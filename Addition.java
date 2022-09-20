a13cd8ca65abf0f460a2d0f9bf30224da1701f7a
bpadave <bhaveshpadave97@gmail.com>
2022-09-20 20:50:08 +0530

    Changes in Addition.java

--- Addition.java
+++ Addition.java
@@ -1,13 +1,6 @@
-import java.*;
-
 public class Addition{
-   
-    public int add(int a, int b){
-        return a+b;        
-    }
     
     public static void main(String[] args) {
-        Sum obj = new Sum();
-        obj.add(2,3);
+    
     }
 }
