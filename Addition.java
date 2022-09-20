664d9fa4d33a11f0ed005fc5fa1fe757c91d2e43
bpadave <bhaveshpadave97@gmail.com>
2022-09-20 10:56:00 +0530

    initial commit

---
+++ Addition.java
@@ -0,0 +1,13 @@
+import java.*;
+
+public class Addition{
+   
+    public int add(int a, int b){
+        return a+b;        
+    }
+    
+    public static void main(String[] args) {
+        Sum obj = new Sum();
+        obj.add(2,3);
+    }
+}
